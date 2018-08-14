72d908cb30a83e76d953f3edb87e2cb994de19c2
Nico R <riesco@gmail.com>
2018-08-14 15:16:17 -0300

    ok

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # clone-data
 Clone JS object data
+
+Based (copy from):
+
+[https://github.com/winstonjs/winston-mongodb/blob/f0095206e9c27e903bd7e896822633bf47601030/lib/helpers.js](https://github.com/winstonjs/winston-mongodb/blob/f0095206e9c27e903bd7e896822633bf47601030/lib/helpers.js)
---
+++ index.js
@@ -0,0 +1,51 @@
+const ObjectID = require('mongodb').ObjectID;
+
+/**
+ * Clones meta object and cleans it from circular references, replacing them
+ * with string '[Circular]' and fixes field names to be storable within
+ * MongoDB
+ * @param {Object} node Current object or its leaf
+ * @param {Array=} optParents Object's parents
+ */
+let cloneMeta = function (node, optParents) {
+  if (!(node instanceof Object) || (node instanceof ObjectID) ||
+      (node instanceof Buffer)) {
+    return node;
+  }
+  let copy = Array.isArray(node) ? [] : {};
+  if (node instanceof Date) {
+    return new Date(node.getTime());
+  } else if (node instanceof Error) {
+    // This is needed because Error's message, name and stack isn't accessible when cycling through properties
+    copy = {
+      message: node.message,
+      name: node.name,
+      stack: node.stack
+    };
+  }
+  optParents = optParents || [];
+  optParents.push(node);
+  for (let key in node) {
+    if (!Object.prototype.hasOwnProperty.call(node, key)) {
+      continue;
+    }
+    let value = node[key];
+    let newKey = key;
+    if (newKey.includes('.') || newKey.includes('$')) {
+      newKey = newKey.replace(/\./g, '[dot]').replace(/\$/g, '[$]');
+    }
+    if (value instanceof Object) {
+      if (optParents.indexOf(value) === -1) {
+        copy[newKey] = cloneMeta(value, optParents);
+      } else {
+        copy[newKey] = '[Circular]';
+      }
+    } else {
+      copy[newKey] = value;
+    }
+  }
+  optParents.pop();
+  return copy;
+};
+
+export default cloneMeta;
